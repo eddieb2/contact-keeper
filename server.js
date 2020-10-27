4de50e3aea26fc00a0bb1eace229db62a416a0a1
Edward Blanciak <blanciak.edward2@gmail.com>
2020-10-27 19:18:34 -0400

    middleware added and mongoDB connection added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// SECTION Connect MongoDB database
+const connectDB = require('./config/db');
+connectDB();
+
+// SECTION Middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
