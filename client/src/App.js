7338bb7df7821d487c06630ee0ffa2d89b524f42
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-02 18:19:50 -0500

    adds register and login forms

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 // SECTION React Context API
 import ContactState from './context/contact/ContactState';
@@ -21,6 +23,8 @@ const App = () => {
 						<Switch>
 							<Route exact path='/' component={Home} />
 							<Route exact path='/about' component={About} />
+							<Route exact path='/register' component={Register} />
+							<Route exact path='/login' component={Login} />
 						</Switch>
 					</div>
 				</Router>
